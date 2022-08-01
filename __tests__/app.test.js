b99cc780e6cbb495b14b098326c98c6d28720a8d
solveigag <solveiga.g@outlook.com>
2022-08-01 16:54:42 +0100

    Add get by id and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,4 +47,41 @@ describe("GET /api/topics", () => {
       });
     })
    })
+});
+
+describe("GET /api/articles/:article_id", () => {
+    describe("STATUS 200", () => {
+        test("responds with status 200 and returns relevant article", () => {
+            return request(app)
+            .get('/api/articles/1')
+            .expect(200)
+            .then((res) => {
+                const date = new Date(1594329060000).toJSON();
+              expect(res.body.article).toEqual({
+                username: 'butter_bridge',
+                article_id: 1,
+                title: "Living in the shadow of a great man",
+                topic: "mitch",
+                body: "I find this existence challenging",
+                created_at: date,
+                votes: 100
+              });
+            });
+        })
+    })
+    describe("STATUS 400s", () => {
+        test("Status 400 - invalid id", () => {
+            return request(app)
+            .get('/api/articles/invalidId')
+            .expect(400)
+            .then((res) => {
+            expect(res.body.msg).toBe('Invalid id');
+            });
+        })
+        test("STATUS 404 - article doesn't exist", () => {
+            return request(app).get('/api/articles/999').expect(404).then((res) => {
+            expect(res.body.msg).toBe("Article doesn't exist");
+             });
+        })
+    })
 })
--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
-const {getTopics} = require('./controllers/controllers')
+const {getTopics, getArticleById} = require('./controllers/controllers')
 
 app.use(express.json()); 
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleById)
+
+
+
 
 app.use((err, req, res, next) => {
+   if (err.code === "22P02") {
+    res.status(400).send({ msg: "Invalid id" });
+   } else {    
+    res.status(err.code).send({ msg: err.msg });
+  }
     
 })
 
