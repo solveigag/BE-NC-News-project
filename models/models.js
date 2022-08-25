65a6dcf38066586a5b6b545d87a6f685cd079a4e
solveigag <solveiga.g@outlook.com>
2022-08-25 12:40:01 +0100

    Add default comment sorting by date in desc order

--- models/models.js
+++ models/models.js
@@ -90,7 +90,7 @@ exports.selectsArticles = (sortBy = "created_at", orderBy = "DESC", topic) => {
 exports.selectAllCommentsByArticleId = (article_id) => {
   return db
     .query(
-      `SELECT comment_id, votes, created_at, author, body FROM comments WHERE article_id = $1`,
+      `SELECT comment_id, votes, created_at, author, body FROM comments WHERE article_id = $1 ORDER BY created_at DESC`,
       [article_id]
     )
     .then(({ rows: allComments }) => {
